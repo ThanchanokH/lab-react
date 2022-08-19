26c4c3da6d97731fac03fce3eb337ad090a76f29
thanchanok hongsuwanwattana <Than2544@hotmail.co.th>
2022-08-19 21:42:14 +0700

    part 7

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
         <center><button className='button' onClick={refreshPage}>Click to reload!</button></center>
         <center><WordCard value="Jaehyun"/></center>
         <center><WordCard value="Taeyong"/></center>
-        <details className='คำใบ้'>
+        <center><details className='คำใบ้'>
           <summary>Hint</summary>
             1.คนที่ cover เพลง I Like Me Better และเป็นสมาชิก nct127<br></br>
             2.คนร้องเพลง long flight และเป็นสมาชิก nct127<br></br>
-        </details>  
+        </details></center>  
     </div>
     
   );
